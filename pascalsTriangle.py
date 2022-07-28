fea3529abcf5e4c8d9d54174f6e346feed699c46
swisskanton <kormanyos.antal@gmail.com>
2022-07-28 13:16:50 +0200

    binomial function is quicker

--- pascalsTriangle.py
+++ pascalsTriangle.py
@@ -1,27 +1,25 @@
 def pascals_triangle(n):
   result = []
-  line = ''
   for i in range(n):
-    line = ''
     for j in range(i + 1 ):
-      line += str(binomial(i, j)) + ', '
       result.append(binomial(i, j))
-    print(line + '\n')
   return result
 
 def binomial(n, k):
-  return int(factorial(n) / (factorial(k) * factorial(n - k)))
+  subresult = 1
+  for i in range(n, n-k, -1):
+    subresult *= i
+  return int(subresult / factorial(k))
 
 def factorial(n):
-  fact = 1
-  for i in range(1, n + 1):
-    fact *= i
-  return fact
-"""
+  if n <= 1:
+    return 1
+  else:
+    return n * factorial(n - 1)
+
 print(pascals_triangle(1), [1],"1 level triangle incorrect")
 print(pascals_triangle(2), [1,1,1],"2 level triangle incorrect")
 print(pascals_triangle(3), [1,1,1,1,2,1],"3 level triangle incorrect")
 print(pascals_triangle(5), [1,1,1,1,2,1,1,3,3,1,1,4,6,4,1],"5 level triangle incorrect")
-"""
-pascals_triangle(60)
+#pascals_triangle(60)
 #print(pascals_triangle(60))
