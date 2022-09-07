bd19ca8bd02b8f4edc295be37d6c08ea3564e483
swisskanton <kormanyos.antal@gmail.com>
2022-09-07 21:42:55 +0200

    finally an accepted solution

--- acmTeam.js
+++ acmTeam.js
@@ -1,32 +1,25 @@
 function acmTeam(topic) {
-    let topicsOfLine = []
+    let max = -Infinity
+    let numbers = new Map()
     for (let i = 0; i < topic.length - 1; i++) {
         for (let j = i + 1; j < topic.length; j++) {
-            
-            topicsOfLine.push((parseInt(topic[i], 2) | parseInt(topic[j], 2)).toString(2).replaceAll('0', '').length)
-                        
-            /*
-            var count = 0
+            let temp = 0 //(parseInt(topic[i], 2) | parseInt(topic[j], 2)).toString(2).replaceAll('0', '').length
             for (let k = 0; k < topic[0].length; k++) {
-                if (parseInt(topic[i].substr(k, 1)) || parseInt(topic[j].substr(k, 1)))
-                    count++
+                temp += parseInt(topic[i][k] | topic[j][k])
             }
-            topicsOfLine.push(count)
-            */
+            if (max < temp)
+                max = temp
+            if (numbers.has(temp)) {
+                numbers.set(temp, numbers.get(temp) + 1)
+            } else {
+                numbers.set(temp, 1)
+            }
         }
     }
-    let maxTopic = Math.max.apply(null, topicsOfLine)
-    var count = 0
-    let index = topicsOfLine.indexOf(maxTopic)
-    //console.log(topicsOfLine, maxTopic)
-    while (index > -1) {
-        count++
-        index = topicsOfLine.indexOf(maxTopic, index + 1)
-    }
-    return [maxTopic, count]
+    return [max, numbers.get(max)]
 }
 
-let topic = ['10101', '11110', '00010']
+topic = ['10101', '11110', '00010']
 console.log('[5, 1]: ' + acmTeam(topic))
 topic = ['10101', '11100', '11010', '00101']
 console.log('[5, 2]: ' + acmTeam(topic))
