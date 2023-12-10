d20b71acdceb6292b4f9dfc445e3ceb8548f4d47
Antal Kormányos <44323201+swisskanton@users.noreply.github.com>
2023-12-10 21:24:12 +0100

    Delete areaOfPolygonInsideCircle.py
    it's uploaded into the Python repository

--- areaOfPolygonInsideCircle.py
+++
@@ -1,7 +0,0 @@
-def area_of_polygon_inside_circle(circle_radius, number_of_sides):
-    import math
-    return round(number_of_sides * circle_radius ** 2 * math.sin(2 * math.pi / number_of_sides) / 2, 3)
-
-print(area_of_polygon_inside_circle(3, 3), '> 11.691')
-print(area_of_polygon_inside_circle(5.8, 7), '> 92.053')
-print(area_of_polygon_inside_circle(4, 5), '> 38.042')
