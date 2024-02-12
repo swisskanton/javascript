2e83434f9f1ad48b00e0282d035d8f9af20084b6
Antal Kormányos <44323201+swisskanton@users.noreply.github.com>
2024-02-12 18:21:00 +0100

    Delete findX.py

--- findX.py
+++
@@ -1,8 +0,0 @@
-def find_x(n):
-    sum_n = int((n - 1) * n / 2)
-    sum_2n = (2 * n - 1) * n
-    return n * (sum_2n + sum_n * 2)
-
-print('16 > ', find_x(2))
-print('63 > ', find_x(3))
-print('325 > ', find_x(5))
