ddc976bc62149a8599e75999d9e126a02ac37305
Antal Kormányos <44323201+swisskanton@users.noreply.github.com>
2024-03-05 00:15:22 +0100

    web component

---
+++ web-component/ExpandableList.js
@@ -0,0 +1,52 @@
+class ExpandableList extends HTMLUListElement {
+    constructor() {
+        super()
+        this.style.position = "relative"
+        this.toggleBtn = document.createElement("button")
+        this.toggleBtn.style.position = "absolute"
+        this.toggleBtn.style.border = "none"
+        this.toggleBtn.style.background = "none"
+        this.toggleBtn.style.padding = 0
+        this.toggleBtn.style.top = 0
+        this.toggleBtn.style.left = "5px"
+        this.toggleBtn.style.cursor = "pointer"
+        this.toggleBtn.innerHTML = ">"
+        this.toggleBtn.addEventListener("click", () => {
+            this.dataset.expanded = !this.isExpanded
+        })
+        this.appendChild(this.toggleBtn)
+
+    }
+
+    get isExpanded() {
+        return this.dataset.expanded !== "false" && this.dataset.expanded != null
+    }
+
+    static get observedAttributes() {
+        return ["data-expanded"]
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        this.updateStyles()
+    }
+
+    connectedCallback() {
+        this.updateStyles()
+    }
+
+    disconnectedCallback() {
+        console.log("disconnected")
+    }
+
+    updateStyles() {
+        const transform = this.isExpanded ? "rotate(90deg" : ""
+        this.toggleBtn.style.transform = transform
+        ;[...this.children].forEach(child => {
+            if (child !== this.toggleBtn) {
+                child.style.display = this.isExpanded ? "" : "none"
+            }
+        })
+    }
+}
+
+customElements.define("expandable-list", ExpandableList, {extends: "ul"})
